8581ac5771e1ea61eabedcb21132728249451c9d
phoenix2life <arb.blogger@gmail.com>
2022-07-31 01:09:18 -0400

    Added depenency_learn() function. Invoked from main() function. Used ferris_says crates.io depenency.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
 fn main() {
     println!("Hello, world!");
+    dependency_learn();
+}
+
+
+fn dependency_learn(){
+    let stdout = stdout();
+    let message = String::from("Hello Fellow Programmers, Welcome to Rust Programming!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+
 }
